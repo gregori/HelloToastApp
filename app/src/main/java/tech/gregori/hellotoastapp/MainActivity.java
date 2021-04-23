b36bbb5ad61f6d089974028ffe3e46c15880d084
Rodrigo Gregori <rodrigo.gregori@gmail.com>
2021-04-22 22:15:56 -0300

    Initial commit

---
+++ app/src/main/java/tech/gregori/hellotoastapp/MainActivity.java
@@ -0,0 +1,46 @@
+package tech.gregori.hellotoastapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int count = 0; // contador
+    private TextView showCount;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showCount = findViewById(R.id.show_count); // atribui elemento à variável showCount
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
+
+    public void showToast(View view) {
+        // criar o Toast com dados específicos
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show(); // mostrar o toast
+    }
+
+    public void countUp(View view) {
+        // quando clicar no botão
+        // incremente o contador
+        count++;
+        // mostre na tela o valor incrementado
+        if (showCount != null) {
+            showCount.setText(Integer.toString(count));
+        }
+    }
+
+    public void countDown(View view) {
+        count--;
+        if (showCount != null) {
+            showCount.setText(Integer.toString(count));
+        }
+    }
+}
