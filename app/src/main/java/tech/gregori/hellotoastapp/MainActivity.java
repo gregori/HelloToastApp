dc845a145a55151b33294617ed09fba7fc4f9115
Rodrigo Gregori <rodrigo.gregori@gmail.com>
2021-05-13 22:15:02 -0300

    Armazenar o estado

--- app/src/main/java/tech/gregori/hellotoastapp/MainActivity.java
+++ app/src/main/java/tech/gregori/hellotoastapp/MainActivity.java
@@ -1,5 +1,6 @@
 package tech.gregori.hellotoastapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
@@ -12,6 +13,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private int count = 0; // contador
     public static final String EXTRA_COUNT = "tech.gregori.hellotoastapp.extra.count";
+    private static final String COUNT = "COUNT";
     private TextView showCount;
 
 
@@ -21,6 +23,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         showCount = findViewById(R.id.show_count); // atribui elemento à variável showCount
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        // Se há conteúdo salvo no InstanceState
+        if (savedInstanceState != null) {
+            // Obtém o valor de count
+            count = savedInstanceState.getInt(COUNT);
+            showCount.setText(Integer.toString(count)); // mostra count no textView
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (count != 0) { // se o contador for diferente de zero (= possui algum valor)
+            outState.putInt(COUNT, count); // armazena no "state"
+        }
     }
 
     public void showToast(View view) {
